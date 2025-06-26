a6cd904140658c9950e2f8c29103ec632d973164
“mohammad-003” <patelmuhammed78@gmail.com>
2025-06-26 13:04:55 +0530

    Reversing string by swaping using 2 pointer

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int left=0;
+        int right = s.length-1;
+
+        while(left< right){
+
+            char temp=s[left];
+            s[left]= s[right];
+            s[right]=temp;
+
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        ReverseString solution = new ReverseString();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(s);
+        System.out.println(s); // Output: ['o', 'l', 'l', 'e', 'h']
+    }
+}
